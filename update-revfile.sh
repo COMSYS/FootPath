a1632bc73d713600b8096bea5abeb9b3c10745c9
Jó Ágila Bitsch <jo.bitsch@cs.rwth-aachen.de>
2011-07-19 00:21:15 +0200

    update-revfile.sh added
    call after commiting so that log files show a reference to the revision
    which was used to create it.

---
+++ update-revfile.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+function parse_git_branch {
+  git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'
+}
+
+function get_git_revision {
+  git rev-parse `parse_git_branch`
+}
+
+
+#echo "branch:" `parse_git_branch`
+#echo "sha1:  " `get_git_revision`
+REVNO=$(get_git_revision)
+echo "package de.uvwxy.footpath; public class Rev{ public final static string rev = \"$REVNO\"; }" > gen/de/uvwxy/footpath/Rev.java
+
+
+
