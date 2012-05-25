2bba71fdcbc53a5609ebc584f2f5376db15aa24e
Helge Reelfs <helge.reelfs@rwth-aachen.de>
2012-05-25 12:19:10 +0200

    changed to using type List instead of LinkedList

--- src/de/uvwxy/footpath2/map/GraphNode.java
+++ src/de/uvwxy/footpath2/map/GraphNode.java
@@ -1,13 +1,13 @@
 package de.uvwxy.footpath2.map;
 
 import java.util.LinkedList;
-
+import java.util.List;
 
 /**
  * A class to maintain a node with given parameters found in OSM/XML data.
  * 
  * @author Paul Smith
- *
+ * 
  */
 public class GraphNode {
 	private double lon;
@@ -19,8 +19,8 @@ public class GraphNode {
 	private int id;
 	private String merge_id;
 	private int numSteps = 0;
-	private LinkedList<GraphEdge> loc_edges;
-	
+	private final List<GraphEdge> loc_edges;
+
 	/**
 	 * Constructor to create an empty node with everything set to 0/null/false
 	 */
@@ -34,16 +34,22 @@ public class GraphNode {
 		this.merge_id = null;
 		loc_edges = new LinkedList<GraphEdge>();
 	}
-	
+
 	/**
 	 * Constructor to create a node with given parameters.
-	 * 	
-	 * @param lon the longitude
-	 * @param lat the latitude
-	 * @param name the name
-	 * @param indoors true if is indoor node
-	 * @param isDoor true if is door
-	 * @param level the level
+	 * 
+	 * @param lon
+	 *            the longitude
+	 * @param lat
+	 *            the latitude
+	 * @param name
+	 *            the name
+	 * @param indoors
+	 *            true if is indoor node
+	 * @param isDoor
+	 *            true if is door
+	 * @param level
+	 *            the level
 	 */
 	public GraphNode(double lon, double lat, String name, boolean indoors, boolean isDoor, float level) {
 		this.lon = lon;
@@ -55,131 +61,132 @@ public class GraphNode {
 		this.merge_id = null;
 		loc_edges = new LinkedList<GraphEdge>();
 	}
-	
+
 	/**
 	 * Creates a LatLonPos object to return the location.
 	 * 
 	 * @return the new LatLonPos object of this node's location
 	 */
-	public LatLonPos getPos(){
+	public LatLonPos getPos() {
 		LatLonPos ret = new LatLonPos();
 		ret.setLat(lat);
 		ret.setLon(lon);
 		ret.setLevel(level);
 		return ret;
 	}
-	
+
 	public double getLon() {
 		return lon;
 	}
-	
+
 	public double getLat() {
 		return lat;
 	}
-	
+
 	public String getName() {
 		return name;
 	}
-	
+
 	public boolean isIndoors() {
 		return isInDoors;
 	}
-	
+
 	public boolean isDoor() {
 		return isDoor;
 	}
-	
+
 	public float getLevel() {
 		return level;
 	}
-	
+
 	public int getId() {
 		return id;
 	}
-	
+
 	public String getMergeId() {
 		return this.merge_id;
 	}
-	
-	public int getSteps(){
+
+	public int getSteps() {
 		return numSteps;
 	}
-	
-	public LinkedList<GraphEdge> getLocEdges(){
+
+	public List<GraphEdge> getLocEdges() {
 		return loc_edges;
 	}
-	
+
 	public void setLon(double lon) {
 		this.lon = lon;
 	}
-	
+
 	public void setLat(double lat) {
 		this.lat = lat;
 	}
-	
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
 	public void setIndoors(boolean indoors) {
 		this.isInDoors = indoors;
 	}
-	
-	public void setDoor(boolean door){
+
+	public void setDoor(boolean door) {
 		this.isDoor = door;
 	}
-	
+
 	public void setLevel(float level) {
 		this.level = level;
 	}
-	
-	public void setId(int id){
+
+	public void setId(int id) {
 		this.id = id;
 	}
-	
+
 	public void setMergeId(String mergeId) {
 		this.merge_id = mergeId;
 	}
-	
-	public void setSteps(int numSteps){
+
+	public void setSteps(int numSteps) {
 		this.numSteps = numSteps;
 	}
-	
-	public boolean equals(GraphNode node){
-		if(node == null)
+
+	public boolean equals(GraphNode node) {
+		if (node == null)
 			return false;
-		return this.getId()==node.getId();
+		return this.getId() == node.getId();
 	}
-	
-	public String toString(){
-		String ret = "\nNode(" + this.id +"): ";
-		ret += name!=null?name:"N/A";
+
+	@Override
+	public String toString() {
+		String ret = "\nNode(" + this.id + "): ";
+		ret += name != null ? name : "N/A";
 		ret += isInDoors ? " (indoors)" : " (outdoors)";
 		ret += "\n    Level: " + this.level;
 		ret += "\n    Lat: " + this.lat;
 		ret += "\n    Lon: " + this.lon;
-		if(getMergeId()!=null){
-			ret +="\n    Merges with: " + getMergeId();
+		if (getMergeId() != null) {
+			ret += "\n    Merges with: " + getMergeId();
 		}
 		return ret;
 	}
-	
-	public String toXML(){
+
+	public String toXML() {
 		String ret = "\n  <node id='" + this.id + "' action='modify' visible='true' ";
 		ret += "lat='" + this.lat + "' lon='" + this.lon + "'>";
 		ret += tag("indoor", this.isInDoors ? "yes" : "no");
 		ret += tag("level", "" + level);
-		if (isDoor){
+		if (isDoor) {
 			ret += tag("highway", "door");
 		}
-		if (name != null && !name.equals("")){
+		if (name != null && !name.equals("")) {
 			ret += tag("name", name);
 		}
 		ret += "\n  </node>";
 		return ret;
 	}
-	
-	private String tag(String k, String v){
+
+	private String tag(String k, String v) {
 		return "\n    <tag k='" + k + "' v='" + v + "' />";
 	}
 }
