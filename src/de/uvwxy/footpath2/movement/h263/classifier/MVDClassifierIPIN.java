8739334e8c6fb7c7eaabfdcfaf815ab3aca23a0f
Paul Smith <code@uvwxy.de>
2012-11-07 01:08:29 +0100

    added mvd classifier for ipin paper version, needs implementation

---
+++ src/de/uvwxy/footpath2/movement/h263/classifier/MVDClassifierIPIN.java
@@ -0,0 +1,11 @@
+package de.uvwxy.footpath2.movement.h263.classifier;
+
+public class MVDClassifierIPIN implements MVDClasssifier {
+
+	@Override
+	public float classify(long now_ms, float[][][] mvds) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
